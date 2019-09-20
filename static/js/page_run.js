d586d8d47eec3f95396f0101c393b76081d6e2c4
Daniel Masake Kitui <daniel.kitui@andela.com>
2019-09-20 16:19:19 +0300

    Add toggle on the file views

--- static/js/page_run.js
+++ static/js/page_run.js
@@ -1,3 +1,14 @@
+
+const expand_dir_view = (event) => {
+	console.log('clicked')
+	console.log('Event: ', event)
+	const div = $(event.currentTarget)
+	
+//	const file_divs = div.getElementsByClassName('file-div')
+	$(event.currentTarget).children('#dir-files').slideToggle()  //show()
+}
+
 $(window).on('load', function() {
 	console.log('We here hommies')
+	$('.info-row').on('click', expand_dir_view);
 });
