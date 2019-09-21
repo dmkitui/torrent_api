2ddc56f7b2a752019223669e85aa1536513fc1cf
Daniel Masake Kitui <daniel.kitui@andela.com>
2019-09-21 09:39:41 +0300

    Add bootbox dialog to display confirmation dialog

--- static/js/page_run.js
+++ static/js/page_run.js
@@ -1,14 +1,41 @@
 
 const expand_dir_view = (event) => {
-	console.log('clicked')
-	console.log('Event: ', event)
 	const div = $(event.currentTarget)
-	
-//	const file_divs = div.getElementsByClassName('file-div')
 	$(event.currentTarget).children('#dir-files').slideToggle()  //show()
 }
 
 $(window).on('load', function() {
 	console.log('We here hommies')
 	$('.info-row').on('click', expand_dir_view);
+	
+	$('.info-row').hover(function(){
+		console.log('Are we hovering?')
+		$(this).find('.delete-btn').show()}, function () {
+			$(this).find('.delete-btn').hide()
+		});
+});
+
+$(document).on("click", ".delete-btn", function(e) {
+	const path = $(this).data('path')
+	console.log('Path: ', path)
+	e.preventDefault()
+	e.stopPropagation();
+	bootbox.confirm({
+	    size: "large",
+			title: 'Confirm Delete File',
+	    message: "Are you sure you want to delete:<br><br><b>" + path + "</b><br><br> This cannot be undone!",
+	    buttons: {
+	        confirm: {
+	            label: 'Delete',
+	            className: 'btn-danger'
+	        },
+	        cancel: {
+	            label: 'Cancel',
+	            className: 'btn-success'
+	        }
+	    },
+	    callback: function(result){
+	    	console.log('User pick: ', result)
+	    }
+	})
 });
